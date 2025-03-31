79f62a36b387c9296c9604f2e1e6921f6870ef94
Pedro Pires <ph_pires@poli.ufrj.br>
2025-03-30 21:31:35 -0300

    Refactoring: organizing code.

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -6,11 +6,6 @@ extends CharacterBody2D
 @onready var force: Vector2
 @onready var ball_res = 6
 
-func apply_collision(plane_normal: Vector2) -> void:
-	var unit_normal = plane_normal.normalized()
-	#var colision_angle = PI - 2 * linear_velocity.angle_to(unit_normal)
-	#linear_velocity = Vector2(cos(colision_angle), sin(colision_angle))
-	
 func get_positive_or_negative():
 	var rng_result = rng.randf_range(-1,1)
 	if rng_result > 0:
@@ -22,9 +17,12 @@ func _ready() -> void:
 	var rng_y = rng.randf_range(90,120)
 	var x_vector_sign = get_positive_or_negative()
 	var y_vector_sign = get_positive_or_negative()
-	velocity = Vector2(x_vector_sign*rng_x, y_vector_sign*rng_y)
+	velocity = Vector2(x_vector_sign * rng_x, y_vector_sign*rng_y)
 	#apply_force(force_magnitude * force)
 
 func _physics_process(delta: float) -> void:
 	position += velocity * delta
+	var collision_info = move_and_collide(velocity * delta)
+	if collision_info:
+		velocity = velocity.bounce(collision_info.get_normal())
 	
---
+++ scripts/board.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+@onready var screen_size = get_viewport_rect().size
+@onready var plane_origin_to_xmax = MyPlane.new(Vector2.ZERO, Vector2(screen_size.x, 0))
+@onready var plane_origin_to_ymax = MyPlane.new(Vector2.ZERO, Vector2(0, screen_size.y))
+@onready var plane_xmax_to_x_and_y_max = MyPlane.new(screen_size, Vector2(screen_size.x, 0))
+@onready var plane_ymax_to_x_and_y_max = MyPlane.new(screen_size, Vector2(0, screen_size.y))
+
+func _physics_process(delta: float) -> void:
+	pass
