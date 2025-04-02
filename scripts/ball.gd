740ed6b9e502dd51ad69e65bb0ea63cb4025ede1
Pedro Pires <ph_pires@poli.ufrj.br>
2025-04-01 22:08:34 -0300

    ball: bouncing :)

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -3,7 +3,6 @@ extends CharacterBody2D
 @export var force_magnitude: int
 
 @onready var rng = RandomNumberGenerator.new()
-@onready var force: Vector2
 @onready var ball_res = 6
 
 func get_positive_or_negative():
@@ -18,11 +17,11 @@ func _ready() -> void:
 	var x_vector_sign = get_positive_or_negative()
 	var y_vector_sign = get_positive_or_negative()
 	velocity = Vector2(x_vector_sign * rng_x, y_vector_sign*rng_y)
-	#apply_force(force_magnitude * force)
 
 func _physics_process(delta: float) -> void:
 	position += velocity * delta
 	var collision_info = move_and_collide(velocity * delta)
 	if collision_info:
+		print(collision_info)
 		velocity = velocity.bounce(collision_info.get_normal())
 	
